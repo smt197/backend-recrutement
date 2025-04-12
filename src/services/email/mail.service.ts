251e004dbae06c283a4c635ba8f896278434594e
smt197 <105449441+smt197@users.noreply.github.com>
2025-04-12 00:27:56 +0000

    mailpit online

--- src/services/email/mail.service.ts
+++ src/services/email/mail.service.ts
@@ -9,13 +9,25 @@ export class MailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: '127.0.0.1', // Remplacez par votre hôte SMTP
-      port: 1025, // Remplacez par votre port SMTP
-      secure: false, // Utilisez true si vous utilisez un port sécurisé
+      host: 'sandbox.smtp.mailtrap.io', // Remplacez par votre hôte SMTP
+      port: 2525,
+      auth: {
+        user: '15852be328df1e',
+        pass: 'ecb3b42e847d72', // Remplacez par votre mot de passe correct
+      },
+      secure: false, // true for 465, false for other ports
+      tls: {
+        rejectUnauthorized: false, // Pour ignorer les erreurs de certificat
+      },
     });
   }
 
-  async sendMail(to: string, subject: string, templateParams: any, templatePath: string) {
+  async sendMail(
+    to: string,
+    subject: string,
+    templateParams: any,
+    templatePath: string,
+  ) {
     const source = fs.readFileSync(templatePath, 'utf8');
     const template = handlebars.compile(source);
     const html = template(templateParams);
@@ -32,21 +44,39 @@ export class MailService {
 
   async sendConfirmationEmail(to: string, name: string, jobTitle: string) {
     const subject = 'Confirmation de soumission de candidature';
-    await this.sendMail(to, subject, { name, jobTitle }, 'src/services/email/confirmation-email-template.hbs');
+    await this.sendMail(
+      to,
+      subject,
+      { name, jobTitle },
+      'src/services/email/confirmation-email-template.hbs',
+    );
   }
 
-  async sendNewJobEmail(to: string, name: string, jobTitle: string, jobDescription: string, jobDeadline: string, jobSkills: string, jobExperience: number) {
+  async sendNewJobEmail(
+    to: string,
+    name: string,
+    jobTitle: string,
+    jobDescription: string,
+    jobDeadline: string,
+    jobSkills: string,
+    jobExperience: number,
+  ) {
     const subject = 'Nouveau poste disponible';
-    await this.sendMail(to, subject, { name, jobTitle, jobDescription, jobDeadline, jobSkills, jobExperience }, 'src/services/email/new-job-email-template.hbs');
+    await this.sendMail(
+      to,
+      subject,
+      { name, jobTitle, jobDescription, jobDeadline, jobSkills, jobExperience },
+      'src/services/email/new-job-email-template.hbs',
+    );
   }
 
   async sendRegistrationEmail(to: string, name: string) {
     const subject = 'Bienvenue sur notre plateforme RH';
     await this.sendMail(
-      to, 
-      subject, 
-      { name, currentDate: new Date().toLocaleDateString('fr-FR') }, 
-      'src/services/email/registration-email-template.hbs'
+      to,
+      subject,
+      { name, currentDate: new Date().toLocaleDateString('fr-FR') },
+      'src/services/email/registration-email-template.hbs',
     );
   }
 }
